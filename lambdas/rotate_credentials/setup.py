f38be6d6ed809b410b89fae91e6b84533998a669
Brad Peters <brad.peters@sonraisecurity.com>
2023-01-09 12:52:28 -0400

    feat: Update secret to include full grapqhl response

--- lambdas/rotate_credentials/setup.py
+++ lambdas/rotate_credentials/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='Sonrai Credential Rotation',
-   version='1.1.0',
+   version='1.2.0',
    description='Sonrai Credential Rotation',
    author='Sonrai Security',
    packages=find_packages()
